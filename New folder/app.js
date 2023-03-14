7b2f8150d771f6f5cf033e9ad7d2ec06ef417b8b
AvaP3 <84456250+avapierini@users.noreply.github.com>
2023-03-14 13:41:19 +0000

    Initial commit

---
+++ New folder/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        showBooks: true,
+        books: [
+          { title: 'name of the wind', author: 'patrick rothfuss' },
+          { title: 'the way of kings', author: 'brandon sanderson' },
+          { title: 'the final empire', author: 'brandon sanderson' },
+        ],
+      }
+    },
+    methods: {
+      toggleShowBooks() {
+        this.showBooks =  !this.showBooks
+      },
+    }
+  })
+  
+  app.mount('#app')
